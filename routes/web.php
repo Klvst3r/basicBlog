4a7a4e51f66d3309118ea10e9e1e861ba36ae3e3
Klvst3r <klvst3r@gmail.com>
2025-01-15 08:59:48 -0600

    4. Rutas iniciales

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request; // Clase que significa solicitud o peticion 
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -11,8 +12,23 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
+* Route::get Consultar
+* Route::post Vamos a laterar nuestra Bd - Guardar
+* Route::delete Funcion Eliminar 
+* Route::put  Actualizar
+|
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Ruta Home';
+});
+
+Route::get('blog', function () {
+    return 'Listado de publicaciones';
+});
+
+
+Route::get('buscar', function (Request $request) {
+    // consulta a BD
+    return $request->all();
 });
