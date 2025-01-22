283f0dc9a539ddb818d5dd93123730d2edd51a3e
Klvst3r <klvst3r@gmail.com>
2025-01-22 10:49:47 -0600

    20. Guardar post en a BD

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
@@ -19,6 +20,24 @@ class PostController extends Controller
         // Retorna a la vists
         return view('posts.create', ['post' => $post ]);
     }
+
+    public function store(Request $request) {
+        //Una nueva publicación a partir del usuario logueado que se enuanetra en la case Request $request
+        //Cremos el registro
+        $post = $request->user()->posts()->create([
+            'title' => $title = $request->title,
+            'slug' => Str::slug($title),
+            'body' => $request->body,
+        ]);
+
+        
+
+        // Retorna a la vists
+        return redirect()->route('posts.edit', $post);
+        
+    }
+
+
     public function edit(Post $post) {
     return view('posts.edit', ['post' => $post ]);
     }
