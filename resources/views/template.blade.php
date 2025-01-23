065ceef87548eccde76ebc77bfb47f57df88739e
Klvst3r <klvst3r@gmail.com>
2025-01-23 15:15:41 -0600

    24 Design

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -5,12 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Proyecto Web</title>
+    
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+
 </head>
 <body>
-    <h1>Blog</h1>
-    <p>
-        <a href="{{ route('home') }}">Home</a>
-        <a href="{{ route('blog') }}">Blog</a>
+    <div class="container px-4 ms-auto">
+        <header class="flex justify-between items-center py-4">
+            <div class="flex items-center flex-grow gap-4">
+                <a href="{{ route('home') }}">
+                    <img src="{{ asset('images/logo.png') }}" class="h-12">
+                </a>
+                <form action="">
+                    <input type="text" placeholder="Buscar">
+                </form>
+            </div>
 
         @auth
             <a href="{{ route('dashboard') }}">Dashboard</a>
@@ -19,8 +31,10 @@
             <a href="{{ route('login') }}">Login</a>
 
         @endauth
-    </p>
-    <hr>
-    @yield('content')
+
+        </header>
+
+        @yield('content')
+    </div>
 </body>
 </html>
