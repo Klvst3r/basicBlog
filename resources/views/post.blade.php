c5737f5951ebd3b971157b8c27ea0150f4caae5b
Klvst3r <klvst3r@gmail.com>
2025-01-27 11:33:01 -0600

    28 Publicación individual

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,13 +1,16 @@
 @extends('template')
 
 @section('content')
+<div class="max-w-3xl mx-auto">
 
-    <title>{{ $post->title }}</title>
 
-    <h1>{{ $post->title }}</h1>
-    <p>{{ $post->body }}</p>
 
-    <a href="{{ url('blog') }}">Volver al listado</a>
+    {{-- <title>{{ $post->title }}</title> --}}
 
+    <h1 class="text-5-xl mb-8">{{ $post->title }}</h1>
+    <p class="leading-loose text-lg text-gray-700">{{ $post->body }}</p>
+
+    {{-- <a href="{{ url('blog') }}">Volver al listado</a> --}}
+</div>
     
 @endsection
