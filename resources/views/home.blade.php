8cd42abb51126e08e2a258251521cca6c2411c36
Klvst3r <klvst3r@gmail.com>
2025-01-27 09:42:25 -0600

    27 Destacado

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,12 @@
 
 @section('content')
     
-    <div>
+    <div class="bg-gray-900 px-20 py-16 rounded-lg mb-8 relative overflow-hidden">
         {{-- Informacion destacada --}}
+        <span class="text-xs uppercase text-gray-700 bg-gray-400 rounded-full px-2 py-1">Programación</span>
+        <h1 class="text-3xl text-white mt-4">Blog</h1>
+        <p class="text-sm text-gray-400 mt">Proyecto de desarrollo web para profesionales</p>
+        <img src="{{ asset('images/dev.png') }}" class="absolute -right-20 -button-20 opacity-20">
     </div>
 
     <div class="px-4">
